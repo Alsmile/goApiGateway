8e2689185fc3e310ba2cb32e09e866159f2c07fd
alsmile <alsmile123@qq.com>
2017-07-14 19:35:25 +0800

    add log

--- db/db.go
+++ db/db.go
@@ -3,13 +3,18 @@ package db
 import (
   "github.com/alsmile/goApiGateway/db/mongo"
   "github.com/alsmile/goApiGateway/db/redis"
+  "log"
 )
 
 func Init() error {
   err := mongo.InitSession()
   if err != nil {
+    log.Printf("[error]mongo connect error: %v\r\n", err)
     return err
   }
   err = redis.NewPool()
+  if err != nil {
+    log.Printf("[error]redis connect error: %v\r\n", err)
+  }
   return err
 }
