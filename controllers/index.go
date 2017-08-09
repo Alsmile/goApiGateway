1a7061d036e2a717adad02255bc40b2f6b11207f
Alsmile <alsmile123@qq.com>
2017-08-09 12:30:07 +0800

    crose

--- controllers/index.go
+++ controllers/index.go
@@ -1,47 +1,55 @@
 package controllers
+
 import (
-  "github.com/kataras/iris"
-  "github.com/kataras/iris/context"
-  "github.com/garyburd/redigo/redis"
-  "github.com/dchest/captcha"
-  "github.com/alsmile/goApiGateway/session"
-  myCaptcha "github.com/alsmile/goApiGateway/services/captcha"
-  "strings"
+	"strings"
+
+	myCaptcha "github.com/alsmile/goApiGateway/services/captcha"
+	"github.com/alsmile/goApiGateway/session"
+	"github.com/dchest/captcha"
+	"github.com/garyburd/redigo/redis"
+	"github.com/kataras/iris"
+	"github.com/kataras/iris/context"
 )
 
 func Index(ctx context.Context) {
-  ctx.StatusCode(iris.StatusOK)
-  ctx.ServeFile("./admin/web/dist/index.html", true)
+	ctx.StatusCode(iris.StatusOK)
+	ctx.ServeFile("./admin/web/dist/index.html", true)
 }
 
 func Browser(ctx context.Context) {
-  ctx.ServeFile("./admin/web/dist/browser.html", true)
+	ctx.ServeFile("./admin/web/dist/browser.html", true)
 }
 
 func NotFound(ctx context.Context) {
-  if strings.HasPrefix(ctx.Path(), "/api/") {
-    ret := make(map[string]interface{})
-    ret["error"] = "请求错误（Not found）：" + ctx.Path()
-    ctx.StatusCode(iris.StatusNotFound)
-    ctx.JSON(ret)
-  } else {
-    ctx.StatusCode(iris.StatusFound)
-    Index(ctx)
-  }
+	if strings.HasPrefix(ctx.Path(), "/api/") {
+		ret := make(map[string]interface{})
+		ret["error"] = "请求错误（Not found）：" + ctx.Path()
+		ctx.StatusCode(iris.StatusNotFound)
+		ctx.JSON(ret)
+	} else {
+		ctx.StatusCode(iris.StatusFound)
+		Index(ctx)
+	}
 }
 
 func Captcha(ctx context.Context) {
-  captchaId, _ := redis.String(session.GetSession(ctx, myCaptcha.CaptchaSessionName))
+	captchaId, _ := redis.String(session.GetSession(ctx, myCaptcha.CaptchaSessionName))
 
-  // Delete the old.
-  if captchaId != "" {
-    captcha.VerifyString(captchaId, "")
-  }
+	// Delete the old.
+	if captchaId != "" {
+		captcha.VerifyString(captchaId, "")
+	}
 
-  captchaId = captcha.New()
-  session.SetSession(ctx, myCaptcha.CaptchaSessionName, captchaId)
-  ctx.Header("Content-Type", "image/png")
-  captcha.WriteImage(ctx.ResponseWriter(), captchaId, 150, 50)
+	captchaId = captcha.New()
+	session.SetSession(ctx, myCaptcha.CaptchaSessionName, captchaId)
+	ctx.Header("Content-Type", "image/png")
+	captcha.WriteImage(ctx.ResponseWriter(), captchaId, 150, 50)
 }
 
+func Cors(ctx context.Context) {
+	ctx.Header("Access-Control-Allow-Origin", "*")
+	ctx.Header("Access-Control-Allow-Headers", "Content-Type, Authorization, token, X-Requested-With")
+	ctx.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
 
+	ctx.Next()
+}
--- routers/main.go
+++ routers/main.go
@@ -1,64 +1,63 @@
 package routers
 
 import (
-  "strconv"
-  "github.com/kataras/iris"
-  "github.com/alsmile/goApiGateway/utils"
-  "github.com/alsmile/goApiGateway/controllers"
-  "github.com/kataras/iris/context"
+	"strconv"
+
+	"github.com/alsmile/goApiGateway/controllers"
+	"github.com/alsmile/goApiGateway/utils"
+	"github.com/kataras/iris"
+	"github.com/kataras/iris/context"
 )
 
 func Start() {
-  app := iris.New()
+	app := iris.New()
 
-  app.OnErrorCode(iris.StatusNotFound, controllers.NotFound)
+	app.OnErrorCode(iris.StatusNotFound, controllers.NotFound)
 
-  admin := app.Party(utils.GlobalConfig.Domain.AdminDomain)
-  {
-    admin.StaticWeb("/assets", "./admin/web/dist/assets")
+	admin := app.Party(utils.GlobalConfig.Domain.AdminDomain)
+	admin.Use(controllers.Cors)
+	{
+		admin.StaticWeb("/assets", "./admin/web/dist/assets")
 
-    admin.Get("/", controllers.Index)
-    admin.Get("/browser.html", controllers.Browser)
-    admin.Get("/captcha", controllers.Captcha)
+		admin.Get("/", controllers.Index)
+		admin.Get("/browser.html", controllers.Browser)
+		admin.Get("/captcha", controllers.Captcha)
 
-    admin.Post("/api/login", controllers.Login)
-    admin.Post("/api/signup", controllers.SignUp)
-    admin.Post("/api/sign/active", controllers.SignActive)
-    admin.Post("/api/forget/password", controllers.ForgetPassword)
-    admin.Post("/api/sign/new/password", controllers.NewPassword)
-    admin.Get("/api/user/profile", controllers.UserProfile)
+		admin.Post("/api/login", controllers.Login)
+		admin.Post("/api/signup", controllers.SignUp)
+		admin.Post("/api/sign/active", controllers.SignActive)
+		admin.Post("/api/forget/password", controllers.ForgetPassword)
+		admin.Post("/api/sign/new/password", controllers.NewPassword)
+		admin.Get("/api/user/profile", controllers.UserProfile)
 
-    admin.Get("/api/sign/config", controllers.GetSignConfig)
+		admin.Get("/api/sign/config", controllers.GetSignConfig)
 
-    admin.Get("/api/site/list", controllers.Auth, controllers.SiteList)
-    admin.Get("/api/site/get", controllers.Auth, controllers.SiteGet)
-    admin.Post("/api/site/save", controllers.Auth, controllers.SiteSave)
-    admin.Post("/api/site/api/save", controllers.Auth, controllers.SiteApiSave)
-    admin.Get("/api/site/api/get", controllers.Auth, controllers.SiteApiGet)
-    admin.Get("/api/site/api/del", controllers.Auth, controllers.SiteApiDel)
-    admin.Get("/api/site/api/list", controllers.Auth, controllers.SiteApiList)
+		admin.Get("/api/site/list", controllers.Auth, controllers.SiteList)
+		admin.Get("/api/site/get", controllers.Auth, controllers.SiteGet)
+		admin.Post("/api/site/save", controllers.Auth, controllers.SiteSave)
+		admin.Post("/api/site/api/save", controllers.Auth, controllers.SiteApiSave)
+		admin.Get("/api/site/api/get", controllers.Auth, controllers.SiteApiGet)
+		admin.Get("/api/site/api/del", controllers.Auth, controllers.SiteApiDel)
+		admin.Get("/api/site/api/list", controllers.Auth, controllers.SiteApiList)
 
-    admin.Post("/api/site/api/list/by/domains", controllers.Auth, controllers.SiteApiListByDomains)
+		admin.Post("/api/site/api/list/by/domains", controllers.Auth, controllers.SiteApiListByDomains)
 
-    admin.Get("/api/test", controllers.ProxyTest)
-    admin.Post("/api/test", controllers.ProxyTest)
-    admin.Put("/api/test", controllers.ProxyTest)
-    admin.Delete("/api/test", controllers.ProxyTest)
+		admin.Get("/api/test", controllers.ProxyTest)
+		admin.Post("/api/test", controllers.ProxyTest)
+		admin.Put("/api/test", controllers.ProxyTest)
+		admin.Delete("/api/test", controllers.ProxyTest)
 
-    admin.Options("/api/{url:path}", func(ctx context.Context) {
-      ctx.Header("Access-Control-Allow-Origin", "*")
-      ctx.Header("Access-Control-Allow-Headers", "Content-Type, Authorization, token, X-Requested-With")
-      ctx.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
-      method := string(ctx.Method())
-      if method == "OPTIONS" {
-        ctx.JSON(true)
-        return
-      }
-    })
-  }
+		admin.Options("/api/{url:path}", func(ctx context.Context) {
+			method := string(ctx.Method())
+			if method == "OPTIONS" {
+				ctx.JSON(true)
+				return
+			}
+		})
+	}
 
-  app.Any("/{url:path}", controllers.ProxyDo)
+	app.Any("/{url:path}", controllers.ProxyDo)
 
-  strPort := strconv.Itoa(int(utils.GlobalConfig.Domain.Port))
-  app.Run(iris.Addr(":" + strPort))
+	strPort := strconv.Itoa(int(utils.GlobalConfig.Domain.Port))
+	app.Run(iris.Addr(":" + strPort))
 }
