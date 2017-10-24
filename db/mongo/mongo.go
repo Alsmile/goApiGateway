093a6381ab7b81c4297cc47511edfc0fafcff4e4
Alsmile <alsmile123@qq.com>
2017-10-24 19:20:24 +0800

    数据库：不认证支持

--- db/mongo/mongo.go
+++ db/mongo/mongo.go
@@ -1,39 +1,42 @@
 package mongo
 
 import (
-  mgo "gopkg.in/mgo.v2"
-  "github.com/alsmile/goApiGateway/utils"
-  "time"
+	"time"
+
+	"github.com/alsmile/goApiGateway/utils"
+	mgo "gopkg.in/mgo.v2"
 )
 
 const (
-  CollectionUsers = "users"
-  CollectionSites = "sites"
-  CollectionApis = "apis"
+	CollectionUsers = "users"
+	CollectionSites = "sites"
+	CollectionApis  = "apis"
 )
 
 var MgoSession *mgo.Session
 
 func InitSession() (err error) {
-  MgoSession, err = mgo.DialWithInfo(&mgo.DialInfo{
-    Addrs: []string{utils.GlobalConfig.Mongo.Address},
-    Username: utils.GlobalConfig.Mongo.User,
-    Password: utils.GlobalConfig.Mongo.Password,
-    Database: utils.GlobalConfig.Mongo.Database,
-    Source: utils.GlobalConfig.Mongo.Database,
-    Mechanism: utils.GlobalConfig.Mongo.Mechanism,
-    PoolLimit: utils.GlobalConfig.Mongo.MaxConnections,
-    Timeout: time.Duration(utils.GlobalConfig.Mongo.Timeout) * time.Second,
-  })
-  if err == nil {
-    if utils.GlobalConfig.Mongo.Debug {
-      mgo.SetDebug(true)
-    }
-    err = MgoSession.DB(utils.GlobalConfig.Mongo.Database).Login(utils.GlobalConfig.Mongo.User, utils.GlobalConfig.Mongo.Password)
-  }
-  if err == nil {
-    MgoSession.SetMode(mgo.Monotonic, true)
-  }
+	MgoSession, err = mgo.DialWithInfo(&mgo.DialInfo{
+		Addrs:     []string{utils.GlobalConfig.Mongo.Address},
+		Username:  utils.GlobalConfig.Mongo.User,
+		Password:  utils.GlobalConfig.Mongo.Password,
+		Database:  utils.GlobalConfig.Mongo.Database,
+		Source:    utils.GlobalConfig.Mongo.Database,
+		Mechanism: utils.GlobalConfig.Mongo.Mechanism,
+		PoolLimit: utils.GlobalConfig.Mongo.MaxConnections,
+		Timeout:   time.Duration(utils.GlobalConfig.Mongo.Timeout) * time.Second,
+	})
+	if err == nil {
+		if utils.GlobalConfig.Mongo.Debug {
+			mgo.SetDebug(true)
+		}
+		if utils.GlobalConfig.Mongo.User != "" {
+			err = MgoSession.DB(utils.GlobalConfig.Mongo.Database).Login(utils.GlobalConfig.Mongo.User, utils.GlobalConfig.Mongo.Password)
+		}
+	}
+	if err == nil {
+		MgoSession.SetMode(mgo.Monotonic, true)
+	}
 
-  return
+	return
 }
--- db/redis/redis.go
+++ db/redis/redis.go
@@ -1,36 +1,39 @@
 package redis
 
 import (
-  "time"
-  "github.com/garyburd/redigo/redis"
-  "github.com/alsmile/goApiGateway/utils"
+	"time"
+
+	"github.com/alsmile/goApiGateway/utils"
+	"github.com/garyburd/redigo/redis"
 )
 
 var RedisPool *redis.Pool
 
 func NewPool() (err error) {
-  RedisPool = &redis.Pool{
-    MaxIdle:     utils.GlobalConfig.Redis.ConnectNum * 2,
-    MaxActive:   utils.GlobalConfig.Redis.ConnectNum,
-    IdleTimeout: time.Duration(utils.GlobalConfig.Redis.Timeout) * time.Second,
-    Dial: func() (redis.Conn, error) {
-      var c redis.Conn
-      c, err = redis.Dial("tcp", utils.GlobalConfig.Redis.Address)
-      if err != nil {
-        return nil, err
-      }
-      if _, err = c.Do("AUTH", utils.GlobalConfig.Redis.Password); err != nil {
-        c.Close()
-        return nil, err
-      }
-      // select db
-      c.Do("SELECT", utils.GlobalConfig.Redis.Db)
-      return c, nil
-    },
-  }
+	RedisPool = &redis.Pool{
+		MaxIdle:     utils.GlobalConfig.Redis.ConnectNum * 2,
+		MaxActive:   utils.GlobalConfig.Redis.ConnectNum,
+		IdleTimeout: time.Duration(utils.GlobalConfig.Redis.Timeout) * time.Second,
+		Dial: func() (redis.Conn, error) {
+			var c redis.Conn
+			c, err = redis.Dial("tcp", utils.GlobalConfig.Redis.Address)
+			if err != nil {
+				return nil, err
+			}
+			if utils.GlobalConfig.Redis.Password != "" {
+				if _, err = c.Do("AUTH", utils.GlobalConfig.Redis.Password); err != nil {
+					c.Close()
+					return nil, err
+				}
+			}
+			// select db
+			c.Do("SELECT", utils.GlobalConfig.Redis.Db)
+			return c, nil
+		},
+	}
 
-  // 调用Get()执行RedisPool.Dial连接redis 。确认连接有效。
-  defer RedisPool.Get().Close()
+	// 调用Get()执行RedisPool.Dial连接redis 。确认连接有效。
+	defer RedisPool.Get().Close()
 
-  return
+	return
 }
